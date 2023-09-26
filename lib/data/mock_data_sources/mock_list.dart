ddff3c06bdcb0c2c969be21c506f79a80742268e
Filip Doganowski <filip.doganowski@gmail.com>
2023-09-26 22:28:26 +0200

    Add mock lists

---
+++ lib/data/mock_data_sources/mock_list.dart
@@ -0,0 +1,46 @@
+import 'package:shot_roulette/domain/models/ingredient_amount_model.dart';
+import 'package:shot_roulette/domain/models/ingredient_model.dart';
+import 'package:shot_roulette/domain/models/ingredient_name_model.dart';
+import 'package:shot_roulette/domain/models/shot_recipe_model.dart';
+import 'package:shot_roulette/domain/models/taste_note_model.dart';
+import 'package:shot_roulette/domain/models/unit_of_measurement_model.dart';
+
+List<ShotRecipeModel> mockRecipes = [
+  const ShotRecipeModel(id: 'id1', title: 'title1', ingredients: [
+    IngredientModel(
+        ingredientNameId: 'ingredientNameId1',
+        amount: IngredientAmountModel(unitId: 'unitId1', amount: 1))
+  ], tasteNoteIds: [
+    'taste1',
+    'taste2'
+  ]),
+];
+
+List<TasteNoteModel> mockTasteNotes = [
+  const TasteNoteModel(
+    id: 'taste1',
+    titleEN: 'titleEN1',
+    titlePL: 'titlePL1',
+  ),
+  const TasteNoteModel(
+    id: 'taste2',
+    titleEN: 'titleEN2',
+    titlePL: 'titlePL2',
+  ),
+];
+
+List<IngredientNameModel> mockIngredientNames = [
+  const IngredientNameModel(
+    id: 'id1',
+    titleEN: 'titleEN',
+    titlePL: 'titlePL',
+  ),
+];
+
+List<UnitOfMeasurementModel> mockUnits = [
+  const UnitOfMeasurementModel(
+    id: 'id1',
+    titleEN: 'titleEN1',
+    titlePL: 'titlePL1',
+  ),
+];
