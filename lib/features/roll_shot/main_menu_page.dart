8f02f5320eedf3486f89d2024279ff1e8e5d5a1f
Filip Doganowski <filip.doganowski@gmail.com>
2024-01-14 17:30:09 +0100

    Extract roll shot page to seperate file

--- lib/features/roll_shot/main_menu_page.dart
+++ lib/features/roll_shot/main_menu_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shot_roulette/features/roll_shot/cubit/roll_shot_cubit.dart';
-import 'package:shot_roulette/features/roll_shot/widgets/roll_shot_button.dart';
-
-import 'package:shot_roulette/features/roll_shot/widgets/shot_recipe_card.dart';
+import 'package:shot_roulette/features/roll_shot/widgets/roll_shot_page.dart';
 
 class MainMenuPage extends StatelessWidget {
   const MainMenuPage({super.key});
@@ -55,54 +53,6 @@ class MainMenuPage extends StatelessWidget {
             }),
           );
         },
-      ),
-    );
-  }
-}
-
-class RollShotPage extends StatelessWidget {
-  const RollShotPage({
-    super.key,
-    required this.mainContainerHeight,
-    required this.state,
-  });
-
-  final double mainContainerHeight;
-  final RollShotState state;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          if (state.chosenRecipe != null)
-            Expanded(
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                child: SizedBox(
-                  height: double.infinity,
-                  child: ShotRecipeCard(state: state),
-                ),
-              ),
-            )
-          else
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: SizedBox(
-                height: mainContainerHeight,
-                width: 150,
-                child: Image.asset('assets/images/casino_roulette.png'),
-              ),
-            ),
-          // Spacer(),
-          RolllShotButton(state: state),
-          const SizedBox(
-            height: 10,
-          ),
-          // if (state.chosenRecipe != null) const ResetButton()
-        ],
       ),
     );
   }
