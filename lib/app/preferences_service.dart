223cf5862173dc5e8f4168771bf5ce553bcf8b7f
Filip Doganowski <filip.doganowski@gmail.com>
2024-01-15 22:41:47 +0100

    Update preferences service

--- lib/app/preferences_service.dart
+++ lib/app/preferences_service.dart
@@ -1,4 +1,7 @@
+import 'package:devicelocale/devicelocale.dart';
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shot_roulette/app/core/constants.dart';
 import 'package:shot_roulette/app/core/enums.dart';
 import 'package:shot_roulette/domain/models/settings_model.dart';
 
@@ -14,13 +17,32 @@ class PreferencesService {
   }
 
   Future getSettings() async {
+    final deviceLocale = await getDeviceLocale();
+
     final preferences = await SharedPreferences.getInstance();
     final selectedTheme =
         SelectedTheme.values[preferences.getInt('theme') ?? 2];
-    final selectedLanguage =
-        SelectedLanguage.values[preferences.getInt('language') ?? 2];
+    final selectedLanguage = SelectedLanguage.values[
+        preferences.getInt('language') ??
+            ((Locale(deviceLocale) == localeList[1]) ? 1 : 0)];
 
     return SettingsModel(
         selectedLanguage: selectedLanguage, selectedTheme: selectedTheme);
+  }
+
+  Future<String> getDeviceLocale() async {
+    final locale = await Devicelocale.currentLocale;
+
+    String language = '';
+
+    if (locale != null && locale.length >= 2) {
+      try {
+        language = locale.substring(0, 2);
+      } catch (e) {
+        debugPrint('Error when fetching user language: $e');
+      }
+    }
+
+    return language;
   }
 }
