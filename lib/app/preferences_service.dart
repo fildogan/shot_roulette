315293c10db71bec34d25e7f85bc11f09bf0d3a7
Filip Doganowski <filip.doganowski@gmail.com>
2024-01-15 17:53:33 +0100

    Add preferences service

---
+++ lib/app/preferences_service.dart
@@ -0,0 +1,26 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shot_roulette/app/core/enums.dart';
+import 'package:shot_roulette/domain/models/settings_model.dart';
+
+class PreferencesService {
+  Future saveTheme(SelectedTheme selectedTheme) async {
+    final preferences = await SharedPreferences.getInstance();
+    await preferences.setInt('theme', selectedTheme.index);
+  }
+
+  Future saveLanguage(SelectedLanguage selectedLanguage) async {
+    final preferences = await SharedPreferences.getInstance();
+    await preferences.setInt('language', selectedLanguage.index);
+  }
+
+  Future getSettings() async {
+    final preferences = await SharedPreferences.getInstance();
+    final selectedTheme =
+        SelectedTheme.values[preferences.getInt('theme') ?? 2];
+    final selectedLanguage =
+        SelectedLanguage.values[preferences.getInt('language') ?? 2];
+
+    return SettingsModel(
+        selectedLanguage: selectedLanguage, selectedTheme: selectedTheme);
+  }
+}
