35777e4c7c574a039ab0c8e5dda11784740c1e84
Filip Doganowski <filip.doganowski@gmail.com>
2024-01-16 18:39:00 +0100

    update preferences service select language logic

--- lib/app/preferences_service.dart
+++ lib/app/preferences_service.dart
@@ -18,15 +18,33 @@ class PreferencesService {
     final preferences = await SharedPreferences.getInstance();
     final selectedTheme =
         SelectedTheme.values[preferences.getInt('theme') ?? 2];
-    final selectedLanguage = SelectedLanguage.values[
-        (preferences.getInt('language') ??
-            ((WidgetsBinding.instance.platformDispatcher.locale
-                    .toString()
-                    .startsWith('pl'))
-                ? 1
-                : 0))];
+    final selectedLanguage = SelectedLanguage
+        .values[(preferences.getInt('language') ?? getSelectedLanguageIndex())];
 
     return SettingsModel(
         selectedLanguage: selectedLanguage, selectedTheme: selectedTheme);
   }
+}
+
+int getSelectedLanguageIndex() {
+  SelectedLanguage selectedLanguage;
+
+  // Convert the input string to lowercase for case-insensitive comparison
+  String deviceLocaleInput =
+      WidgetsBinding.instance.platformDispatcher.locale.toString();
+
+  // Iterate over all enum values
+  for (SelectedLanguage language in SelectedLanguage.values) {
+    // Convert the enum value to lowercase for comparison
+    String enumString = language.toString().split('.')[1].toLowerCase();
+
+    // Check if the input matches the lowercase string representation of the enum value
+    if (deviceLocaleInput.startsWith(enumString)) {
+      selectedLanguage = language;
+      return selectedLanguage.index;
+    }
+  }
+
+  // Handle the case where the input does not match any enum value
+  return 0;
 }
