8c2a5c2b20c7da2ea242f58e6594406b0cac256c
Filip Doganowski <filip.doganowski@gmail.com>
2024-01-16 18:16:35 +0100

    Update assign language at first start app

--- lib/app/preferences_service.dart
+++ lib/app/preferences_service.dart
@@ -1,7 +1,5 @@
-import 'package:devicelocale/devicelocale.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:shot_roulette/app/core/constants.dart';
 import 'package:shot_roulette/app/core/enums.dart';
 import 'package:shot_roulette/domain/models/settings_model.dart';
 
@@ -17,43 +15,18 @@ class PreferencesService {
   }
 
   Future getSettings() async {
-    final deviceLocale = await getDeviceLocale();
-
     final preferences = await SharedPreferences.getInstance();
     final selectedTheme =
         SelectedTheme.values[preferences.getInt('theme') ?? 2];
     final selectedLanguage = SelectedLanguage.values[
-        preferences.getInt('language') ??
-            ((Locale(deviceLocale) == localeList[1]) ? 1 : 0)];
+        (preferences.getInt('language') ??
+            ((WidgetsBinding.instance.platformDispatcher.locale
+                    .toString()
+                    .startsWith('pl'))
+                ? 1
+                : 0))];
 
     return SettingsModel(
         selectedLanguage: selectedLanguage, selectedTheme: selectedTheme);
-  }
-
-  Future<String> getDeviceLocale() async {
-    final locale = await Devicelocale.currentLocale;
-
-    String language = '';
-    String country = '';
-
-    if (locale != null && locale.length >= 2) {
-      try {
-        language = locale.substring(0, 2);
-      } catch (e) {
-        debugPrint('Error when fetching user language: $e');
-      }
-    }
-
-    if (locale != null && locale.length >= 5) {
-      try {
-        country = locale.substring(3, 5);
-      } catch (e) {
-        debugPrint('Error when fetching user country: $e');
-      }
-    }
-
-    print(language);
-    print(country);
-    return language;
   }
 }
