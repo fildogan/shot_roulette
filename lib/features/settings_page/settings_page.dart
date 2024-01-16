8f680ed2680d34e5f6f56f35ebb111109849a84d
Filip Doganowski <filip.doganowski@gmail.com>
2024-01-16 10:00:13 +0100

    Add theme selection page

--- lib/features/settings_page/settings_page.dart
+++ lib/features/settings_page/settings_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shot_roulette/domain/models/setting_item_model.dart';
 import 'package:shot_roulette/features/roll_shot/cubit/roll_shot_cubit.dart';
+import 'package:shot_roulette/features/settings_page/pages/theme_selection.dart';
 import 'package:shot_roulette/features/settings_page/widgets/settings_item.dart';
 
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -16,43 +18,52 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final localizations = AppLocalizations.of(context)!;
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SettingsItem(
-              header: localizations.application,
-              settingsItems: [
-                SettingItemModel(
-                  title: localizations.theme,
-                  onTap: () {
-                    // Navigator.of(context).push(
-                    //   MaterialPageRoute(
-                    //     builder: (context) => const ThemeSelectionPage(),
-                    //   ),
-                    // );
-                  },
-                  trailing: const Icon(Icons.chevron_right),
-                ),
-                SettingItemModel(
-                  title: localizations.language,
-                  onTap: () {
-                    // Navigator.of(context).push(
-                    //   MaterialPageRoute(
-                    //     builder: (context) =>
-                    //         const LanguageSelectionPage(),
-                    //   ),
-                    // );
-                  },
-                  trailing: const Icon(Icons.chevron_right),
-                ),
-              ],
-            ),
-          ],
+
+    if (state.settingsMenuPage != null) {
+      return state.settingsMenuPage!;
+    } else {
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SettingsItem(
+                header: localizations.application,
+                settingsItems: [
+                  SettingItemModel(
+                    title: localizations.theme,
+                    onTap: () {
+                      context
+                          .read<RollShotCubit>()
+                          .changeSettingsPage(ThemeSelectionPage(state: state));
+
+                      // Navigator.of(context).push(
+                      //   MaterialPageRoute(
+                      //     builder: (context) => const ThemeSelectionPage(),
+                      //   ),
+                      // );
+                    },
+                    trailing: const Icon(Icons.chevron_right),
+                  ),
+                  SettingItemModel(
+                    title: localizations.language,
+                    onTap: () {
+                      // Navigator.of(context).push(
+                      //   MaterialPageRoute(
+                      //     builder: (context) =>
+                      //         const LanguageSelectionPage(),
+                      //   ),
+                      // );
+                    },
+                    trailing: const Icon(Icons.chevron_right),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    }
   }
 }
