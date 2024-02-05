ab4fdd5cdf5067b78eddc86d5b0e94adc073f141
Filip Doganowski <filip.doganowski@gmail.com>
2024-02-05 22:38:29 +0100

    Add circular progress indicator at first sign in

--- lib/features/auth/pages/auth_home.dart
+++ lib/features/auth/pages/auth_home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:shot_roulette/app/core/enums.dart';
 import 'package:shot_roulette/features/auth/cubit/auth_page_cubit.dart';
 import 'package:shot_roulette/features/cocktail_page/widgets/custom_main_button.dart';
 
@@ -14,29 +15,31 @@ class AuthHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final localizations = AppLocalizations.of(context)!;
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CustomMainButton(
-            onPressed: () {
-              context.read<AuthPageCubit>().goLogInPage();
-            },
-            title: 'Log In',
-          ),
-          CustomMainButton(
-            onPressed: () {
-              context.read<AuthPageCubit>().goSignUp();
-            },
-            title: 'Sign Up',
-          ),
-          CustomMainButton(
-            onPressed: () {
-              context.read<AuthPageCubit>().signInAnonymously();
-            },
-            title: 'Anonymous',
-          ),
-        ],
-      ),
+      child: state.authStatus == Status.loading
+          ? const CircularProgressIndicator()
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CustomMainButton(
+                  onPressed: () {
+                    context.read<AuthPageCubit>().goLogInPage();
+                  },
+                  title: 'Log In',
+                ),
+                CustomMainButton(
+                  onPressed: () {
+                    context.read<AuthPageCubit>().goSignUp();
+                  },
+                  title: 'Sign Up',
+                ),
+                CustomMainButton(
+                  onPressed: () {
+                    context.read<AuthPageCubit>().signInAnonymously();
+                  },
+                  title: 'Anonymous',
+                ),
+              ],
+            ),
     );
   }
 }
