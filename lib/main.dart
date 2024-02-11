33ef92a404bb5739b59433f7cd0150e0e87ab9cb
Filip Doganowski <filip.doganowski@gmail.com>
2024-02-11 23:31:36 +0100

    set OPEN_AI_API_KEY with dart_openai

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:dart_openai/dart_openai.dart';
+
 import 'package:shot_roulette/app/app.dart';
 import 'package:shot_roulette/app/injection_container.dart';
 import 'package:shot_roulette/firebase_options.dart';
 
 void main() async {
-  await dotenv.load(fileName: ".env");
-  configureDependencies();
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  OpenAI.apiKey = dotenv.env['OPEN_AI_API_KEY'] ?? '';
+  configureDependencies();
   runApp(const MyApp());
 }
